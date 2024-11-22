e4832f825967548e95d4d72adfc068c8588c17ba
bella <bellawmm69@gmail.com>
2024-11-22 15:03:19 +0800

    feat: fix bugs

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,11 @@
+import React from "react";
 import { Box, Flex, Image } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Header from "../components/Header";
-import lottie from "lottie-web";
-import { useEffect } from "react";
 import MaskLeft from '../assets/mask_l.png'
-import MaskRight from '../assets/mask_r.png'
 const Root = () => {
   return (
-    <Flex w="100vw" minH={'100vh'} flexDir={"column"}  fontFamily={'Alata'} gap={3} background={'#F9FBF8'} overflow={'hidden'}>
+    <Flex overflowX={'hidden'} w="100vw" minH={'100vh'} flexDir={"column"}  fontFamily={'Alata'} gap={3} background={'#F9FBF8'} overflow={'hidden'}>
       
       <Header />
       <Box width={'25.75%'} position={'absolute'} left={0} bottom={0}>
